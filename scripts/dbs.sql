e04b50b7555866b2632100ad89b7503d49d18ad5
balusai1 <sseashu1@gmail.com>
2018-11-30 14:27:49 +0530

    Update dbs.sql

--- scripts/dbs.sql
+++ scripts/dbs.sql
@@ -5,6 +5,8 @@ CREATE DATABASE hue;
 CREATE DATABASE reportmanager;
 
 
+
+
 CREATE USER 'hive'@'%' IDENTIFIED BY 'Itv3rs1ty';
 CREATE USER 'oozie'@'%' IDENTIFIED BY 'Itv3rs1ty';
 CREATE USER 'hue'@'%' IDENTIFIED BY 'Itv3rs1ty';
