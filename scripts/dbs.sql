e1f3e85db0868e60f31b478dab417e1e5f7d6937
BALUSAI <balusai@sai.seashu@gmail.com>
2018-11-30 14:15:42 +0530

    Adding scripts

---
+++ scripts/dbs.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE scm;
+CREATE DATABASE hive;
+CREATE DATABASE oozie;
+CREATE DATABASE hue;
+CREATE DATABASE reportmanager;
+
+
+CREATE USER 'hive'@'%' IDENTIFIED BY 'Itv3rs1ty';
+CREATE USER 'oozie'@'%' IDENTIFIED BY 'Itv3rs1ty';
+CREATE USER 'hue'@'%' IDENTIFIED BY 'Itv3rs1ty';
+CREATE USER 'rm'@'%' IDENTIFIED BY 'Itv3rs1ty';
+CREATE USER 'scm'@'%' IDENTIFIED BY 'Itv3rs1ty';
+
+GRANT ALL PRIVILEGES ON scm.* TO 'scm'@'%';
+GRANT ALL PRIVILEGES ON hive.* TO 'hive'@'%';
+GRANT ALL PRIVILEGES ON oozie.* TO 'oozie'@'%';
+GRANT ALL PRIVILEGES ON hue.* TO 'hue'@'%';
+GRANT ALL PRIVILEGES ON reportmanager.* TO 'rm'@'%';
+
+FLUSH PRIVILEGES;
